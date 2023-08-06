a78b0d10f7873c00842feac17013e9bb051c8491
shun2k <gerogeroshocker@gmail.com>
2023-08-06 23:39:07 +0900

    add services

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "loto7s#index"
+  root 'services#index'
+  get 'services/show', to: 'services#show'
+  get 'services/search', to: 'services#search'
+  get 'services/data', to: 'services#data'
 end
