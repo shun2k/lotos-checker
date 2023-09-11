2653fa6afe099014e32bb32c740cf39ad7c1a1d3
shun2k <gerogeroshocker@gmail.com>
2023-09-11 16:51:28 +0900

    fix previous-data layout

---
+++ app/javascript/controllers/table_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.handleResize();
+    window.addEventListener("resize", this.handleResize.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener("resize", this.handleResize.bind(this));
+  }
+
+  handleResize() {
+    const tableHeaders = document.querySelectorAll('th');
+    const tableCells = document.querySelectorAll('td');
+    const alwaysVisibleElements = document.querySelectorAll('.always-visible');
+
+    if (window.innerWidth >= 768) {
+      tableHeaders.forEach((header) => {
+        header.classList.remove('hidden');
+      });
+      tableCells.forEach((cell) => {
+        cell.classList.remove('hidden');
+      });
+    } else {
+      tableHeaders.forEach((header) => {
+        header.classList.add('hidden');
+      });
+      tableCells.forEach((cell) => {
+        cell.classList.add('hidden');
+      });
+    }
+
+    if (window.innerWidth < 768) {
+      alwaysVisibleElements.forEach((element) => {
+        element.classList.remove('hidden');
+      });
+    // } else {
+    //   alwaysVisibleElements.forEach((element) => {
+    //     element.classList.add('hidden');
+    //   });
+    }
+  }
+}
