c9b9163bcf988ca6f7f5b1331e2462c7fd6da198
shun2k <gerogeroshocker@gmail.com>
2023-09-19 00:23:40 +0900

    fix factory_bot errors

---
+++ spec/factories/loto7.rb
@@ -0,0 +1,22 @@
+
+FactoryBot.define do
+  factory :loto7 do
+    times { "1" }
+    first { "7" }
+    second { "10" }
+    third { "12" }
+    forth { "17" }
+    fifth { "23" }
+    sixth { "28" }
+    seventh { "34" }
+    bonusfirst { "3" }
+    bonussecond { "15" }
+    announcement { "2013-04-05 00:00:00 +0900" }
+    firstcounts { "1" }
+    secondcounts { "14" }
+    thirdcounts { "168" }
+    forthcounts { "8254" }
+    fifthcounts { "115022" }
+    sixthcounts { "189593" }
+  end
+end
