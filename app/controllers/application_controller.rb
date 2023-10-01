44e9afffbe24c0808f2b6b9910c39fe99b274131
岸本俊祐 <kishimotoshunyuu@kishimotoshunyuunoMacBook-Air.local>
2023-10-02 00:32:22 +0900

    modified admin layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    loto7s_path  #ここを好きなパスに変更
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path #ここを好きなパスに変更
+  end
+
 end
